cd6a9618f0a6458f6a661b3b528a850df127e002
fyardlest <fyardlest1@gmail.com>
2025-05-25 11:10:38 -0400

    add description to the script section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
+<!-- first part is the setup -->
+<script setup>
+import { ref } from 'vue';
+const product = ref('Boots');
+const description = ref('Beautiful boots for the summer');
+</script>
 
+<!-- second part is the template -->
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-info">
-        <h1>Socks</h1>
-      </div>
-    </div>
-  </div>
+	<div class="product-display">
+		<div class="product-container">
+			<div class="product-info">
+				<h1>{{ product }}</h1>
+				<p>{{ description }}</p>
+			</div>
+		</div>
+	</div>
 </template>
